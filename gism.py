b6c8b179d6c1a174539cdee7f4b63317207f9a21
rain <rain@gmx.tm>
2022-06-29 14:20:00 +0300

    Removed old configuration modules and some unused code; get_weather_g moved to gism.py

--- gism.py
+++ gism.py
@@ -1,23 +1,157 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import pogoda_g_plugin
-
 wz_cache = {'public':{}, 'private':{}}
 cache_ttl = 600
 wz_clients = {}
 
-version = 'unknown'
+version = '0.1'
 
-import sha
-#import email, os, signal, smtplib, sys, time, traceback, xmpp
-import os, signal, sys, time, traceback, xmpp, md5
-from xmpp.browser import *
-#from email.MIMEText import MIMEText
-#from email.Header import decode_header
-import config, xmlconfig
+import os
+import signal
+import sys
+import time
+import traceback
+import xmpp
 import threading
+import urllib2
+import xml.dom.minidom
 
+from xmpp.browser import *
+from xml.parsers import expat
+
+config=os.path.abspath(os.path.dirname(sys.argv[0]))+'/config.xml'
+
+dom = xml.dom.minidom.parse(config)
+
+NAME =  dom.getElementsByTagName("name")[0].childNodes[0].data
+HOST =  dom.getElementsByTagName("host")[0].childNodes[0].data
+PORT =  dom.getElementsByTagName("port")[0].childNodes[0].data
+PASSWORD = dom.getElementsByTagName("password")[0].childNodes[0].data
+
+def get_weather_g(kod, type):
+			kod=str(kod)
+			req = urllib2.Request(u'http://informer.gismeteo.ru/xml/'+kod+u'_1.xml',headers={'User-agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'})
+#			req = urllib2.Request(u'http://localhost/xml/'+kod+u'_1.xml')
+			r = urllib2.urlopen(req)
+
+			global embedd
+			embedd=0
+			global wz,period
+			wz={}
+			period=0
+
+			def start_element(name, attrs):
+				global embedd,wz,period
+#				prefix=''
+#				for x in range(embedd):
+#					prefix+='\t'
+#				print 'start_element:',prefix, name, attrs
+				embedd+=1
+				if period not in wz:
+					wz[period]={}
+				wz[period][name]=attrs
+
+			def end_element(name):
+				global embedd,period
+				embedd-=1
+				if name=='FORECAST':
+#					print 'fore'#,period
+					period+=1
+				return
+#				prefix=''
+#				for x in range(embedd):
+#					prefix+='\t'
+#				print 'end_element:',prefix, name
+#			def char_data(data):
+#				print 'char_data:', repr(data)
+
+			p = expat.ParserCreate()
+
+			p.StartElementHandler = start_element
+			p.EndElementHandler = end_element
+#			p.CharacterDataHandler = char_data
+			try:
+				p.ParseFile(r)
+			except:
+#			reply(type ,source, u'Нет данных...')
+				return None
+
+			cityname=urllib2.unquote(str(wz[0][u'TOWN'][u'sname'])).decode("cp1251")
+
+#			return
+			weather = u'Погода по г. '+cityname+u' (№'+kod+u'):'
+			weather_s = u'Погода по г. '+cityname+u' (№'+kod+u'):'
+			weather_sms =u'№'+kod+u':'
+			for period in wz:
+				pr=wz[period]
+#				try:
+				if 1:
+#					print pr.keys()
+#					print pr[u'TOWN'].keys()
+#					print pr[u'TOWN'][u'sname']
+					day = str(pr[u'FORECAST'][u'day'])
+					mounth = pr[u'FORECAST'][u'month']
+					hour = pr[u'FORECAST'][u'hour']
+					week = pr[u'FORECAST'][u'weekday']
+					cloud = pr[u'PHENOMENA'][u'cloudiness']
+					precipitation = pr[u'PHENOMENA'][u'precipitation']
+					presmax = pr[u'PRESSURE'][u'max']
+					presmin = pr[u'PRESSURE'][u'min']
+					tempmax = pr[u'TEMPERATURE'][u'max']
+					tempmin = pr[u'TEMPERATURE'][u'min']
+					heatmax = pr[u'HEAT'][u'max']
+					heatmin = pr[u'HEAT'][u'min']
+					windmin = pr[u'WIND'][u'min']
+					windmax = pr[u'WIND'][u'max']
+					winddir = pr[u'WIND'][u'direction']
+					rewmax = pr[u'RELWET'][u'max']
+					rewmin = pr[u'RELWET'][u'min']
+#				except:
+#					print 'can\'t parse'
+
+				try:
+					hour=int(hour)
+					if hour in range(0,6):
+						hour=u'Ночь'
+					elif  hour in range(6,12):
+						hour=u'Утро'
+					elif  hour in range(12,18):
+						hour=u'День'
+					elif  hour in range(18,24):
+						hour=u'Вечер'
+				except:
+					print 'can\'t parse'
+					pass
+
+
+				months=[u'ХЗраля',u'Января',u'Февраля',u'Марта',u'Апреля',u'Мая',u'Июня',u'Июля',u'Августа',u'Сентября',u'Октября',u'Ноября',u'Декабря']
+				weekdays=[u'Нулесенье 8-|',u'Воскресенье',u'Понедельник',u'Вторник',u'Среда',u'Четверг',u'Пятница',u'Суббота',u'Восьмесение О_о']
+				mounth=months[int(mounth)]
+				week=weekdays[int(week)]
+
+				wind=[u'северный',u'северо-восточный',u'восточный',u'юго-восточный',u'южный',u'юго-западный',u'западный',u'северо-западный']
+				sky=[u'ясно',u'малооблачно',u'облачно',u'пасмурно',u'дождь',u'ливень',u'снег',u'снег',u'гроза',u'нет данных',u'без осадков']
+				skys=[u'ясн',u'малоб',u'обл',u'пасм',u'дожд',u'ливен',u'снег',u'снег',u'гроза',u'???',u'б/осад']
+
+				winddir = wind[int(winddir)]
+				clouds=skys[int(cloud)]
+				cloud=sky[int(cloud)]
+				precipitations=skys[int(precipitation)]
+				precipitation=sky[int(precipitation)]
+
+				weather += '\n'+hour+ u' (' +day+ ' ' +mounth+ ', '+week+u'):\n  температура воздуха от '+tempmin+ u' до '+tempmax+u' ('+heatmin+u'-'+heatmax+u')'+u';\n  '+cloud+u', '+precipitation+u';\n  атмосферное давление '+presmin+u'-'+presmax+u'мм.рт.ст.;\n  ветер '+winddir+ u', '+windmin+'-'+windmax+u'м/с;\n  влажность воздуха '+rewmin+'-'+rewmax+u'%;\n'
+				weather_s += '\n'+hour+u' '+tempmin+ u'..'+tempmax+u' ('+heatmin+u'..'+heatmax+u')'+u'; '+cloud+u', '+precipitation
+				weather_sms += '\n'+hour[0]+tempmin+ u'-'+tempmax+u'['+heatmin+u'-'+heatmax+u']'+u';'+clouds+u','+precipitations
+
+			if type == 'public':
+				return weather_s
+			if type == 'private':
+				return weather
+			if type == 'sms':
+				return weather_sms
+			else:
+				return weather
 
 def get_gism(node, short):
 	pub = 'public' if short else 'private'
@@ -29,7 +163,7 @@ def get_gism(node, short):
 	if (code in wz_cache[pub]) and (time.time() - wz_cache[pub][code][0] < cache_ttl):
 			wz = wz_cache[pub][code][1]
 	else:
-			wz_cache[pub][code] = time.time(),pogoda_g_plugin.get_weather_g(code,pub)
+			wz_cache[pub][code] = time.time(), get_weather_g(code,pub)
 	return wz_cache[pub][code][1]
 
 
@@ -41,18 +175,17 @@ class Transport:
 
 	def __init__(self,jabber):
 		self.jabber = jabber
-		self.watchdir = config.watchDir
-		self.domain = config.mainServerJID
+		self.domain = NAME
 		self.usercfg_load()
 		self.msglock = threading.Lock()
-		if '~' in self.watchdir:
-			self.watchdir = self.watchdir.replace('~', os.environ['HOME'])
+#		if '~' in self.watchdir:
+#			self.watchdir = self.watchdir.replace('~', os.environ['HOME'])
 		# A list of two element lists, 1st is xmpp domain, 2nd is email domain
 		#self.mappings = [mapping.split('=') for mapping in config.domains]
 		#email.Charset.add_charset( 'utf-8', email.Charset.SHORTEST, None, None )
 		self.Features = {
 						'ids':[
-							{'category':'presence','type':'text','name':config.discoName}],
+							{'category':'presence','type':'text','name':"Gismeteo Weather Service"}],
 						'features':[NS_VERSION,NS_COMMANDS,NS_GATEWAY,NS_REGISTER,NS_DISCO_INFO]}
 		self.online_users = {}
 
@@ -62,8 +195,8 @@ class Transport:
 		self.jabber.RegisterHandler('iq',      self.xmpp_iq)
 		self.disco = Browser()
 		self.disco.PlugIn(self.jabber)
-		self.disco.setDiscoHandler(self.xmpp_base_disco,node='',jid=config.jid)
-		
+		self.disco.setDiscoHandler(self.xmpp_base_disco,node='',jid=NAME)
+
 	# IQ Handlers
 	def xmpp_iq(self, conn, iq):
 		print '======================= iq: ',[self], [conn], [iq]
@@ -102,7 +235,7 @@ class Transport:
 		else:
 			print "***************************************",ns
 			self.send_not_implemented(iq)
-		
+
 	def iq_disco_info_handler(self, iq):
 		jid_from = iq.getFrom()
 		jid_from_stripped = jid_from.getStripped()
@@ -112,13 +245,13 @@ class Transport:
 		_id = iq.getAttr('id')
 		node = iq.getTagAttr('query','node')
 		print "INFO",jid_from, jid_to, typ, _id, node
-		if jid_to_stripped==config.jid and typ=='get' and ((not node) or (node == '#'.join((utils.NODE, utils.SERVER_CAPS)))):
+		if jid_to_stripped==NAME and typ=='get' and ((not node) or (node == '#'.join((utils.NODE, utils.SERVER_CAPS)))):
 			#If node is empty or equal NODE#SERVER_CAPS then returm disco
 			reply = iq.buildReply(typ='result')
 			reply.setQueryPayload(self.Features)
 			self.jabber.send(reply)
 		print "INFO2",jid_from, jid_to, typ, _id, node
-		
+
 	def iq_register_handler(self, iq):
 		jid_from = iq.getFrom()
 		jid_from_stripped = jid_from.getStripped()
@@ -127,13 +260,13 @@ class Transport:
 		typ = iq.getType()
 		iq_children = iq.getQueryChildren()
 		print "REGISTER ",jid_from, jid_to, typ, iq_children
-		if (typ=='get') and (jid_to_stripped==config.jid) and (not iq_children):
+		if (typ=='get') and (jid_to_stripped==NAME) and (not iq_children):
 			repl = iq.buildReply(typ='result')
 			repl.setQueryPayload(self.get_register_form(jid_from_stripped))
 			self.jabber.send(repl)
 			raise NodeProcessed
 		#elif typ == 'set' and (jid_to_stripped==config.jid) and iq_children:
-		elif typ == 'set' and (jid_to_stripped==config.jid): #to additional commands use ^^^
+		elif typ == 'set' and (jid_to_stripped==NAME): #to additional commands use ^^^
 			query_tag = iq.getTag('query')
 			#if 1:#query_tag.getTag('email') and query_tag.getTag('password'):
 			if query_tag.getTag('nick'):
@@ -163,17 +296,16 @@ class Transport:
 				'''
 				repl = iq.buildReply(typ='result')
 				self.jabber.send(repl)
-				sub = xmpp.Presence(to=jid_from_stripped,frm=config.jid)
+				sub = xmpp.Presence(to=jid_from_stripped,frm=NAME)
 				sub.setType('subscribe')
 				self.jabber.send(sub)
 				print "REGISTERED with",jid_from_stripped
-				
-				md = md5.md5(jid_from.getStripped()).hexdigest()
+
 				m = Message(to=jid_from,frm = jid_to, body = 'Registration successful. Use JIDs like NUMBER@gismeteo.jabbercity.ru, where number is gismeteo city code, for example, 34731')
 				self.jabber.send(m)
-				
+
 				raise NodeProcessed
-	
+
 			elif query_tag.getTag('remove'):
 				#account = spool.Profile(jid_from_stripped)
 				if 1:#account.remove():
@@ -181,18 +313,18 @@ class Transport:
 					ok_iq = iq.buildReply(typ='result')
 					ok_iq.setPayload([],add=0)
 					self.jabber.send(ok_iq)
-					unsub = xmpp.Presence(to=jid_from_stripped,frm=config.jid)
+					unsub = xmpp.Presence(to=jid_from_stripped,frm=NAME)
 					unsub.setType('unsubscribe')
 					self.jabber.send(unsub)
 					unsub.setType('unsubscribed')
 					self.jabber.send(unsub)
-					
+
 					print "RAISE!!!"
 					raise NodeProcessed
 				else:
 					pass
 
-					
+
 	def get_register_form(self, jid):
 		user = ''#'spool.Profile(jid).getUsername()'
 		instr = xmpp.Node('instructions')
@@ -208,13 +340,13 @@ class Transport:
 			#email.setData('fake')
 			return [instr,nick]
 			return [instr,email]
-		
+
 	def iq_gateway_handler(self, iq):
 		jid_to = iq.getTo()
 		jid_to_stripped = jid_to.getStripped()
 		iq_children = iq.getQueryChildren()
 		typ = iq.getType()
-		if (typ=='get') and (jid_to_stripped==config.jid) and (not iq_children):
+		if (typ=='get') and (jid_to_stripped==NAME) and (not iq_children):
 			repl = iq.buildReply(typ='result')
 			query = xmpp.Node('query', attrs={'xmlns':xmpp.NS_GATEWAY})
 #			query.setTagData('desc', i18n.ENTER_EMAIL)
@@ -222,12 +354,12 @@ class Transport:
 			query.setTag('prompt')
 			repl.setPayload([query])
 			self.jabber.send(repl)
-		elif (typ=='set') and (jid_to_stripped==config.jid) and iq_children:
+		elif (typ=='set') and (jid_to_stripped==NAME) and iq_children:
 			e_mail = [node.getData() for node in iq_children if node.getName()=='prompt']
 			if len(e_mail) == 1:
 				prompt = xmpp.simplexml.Node('jid')
 #				prompt.setData(utils.mail2jid(e_mail[0]))
-				prompt.setData(e_mail[0].replace('@', '%') + '@' + config.jid)
+				prompt.setData(e_mail[0].replace('@', '%') + '@' + NAME)
 				repl = iq.buildReply(typ='result')
 				repl.setQueryPayload([prompt])
 				self.jabber.send(repl)
@@ -235,7 +367,7 @@ class Transport:
 				self.jabber.send_bad_request(iq)
 		else:
 			self.jabber.send_not_implemented(iq)
-			
+
 	def send_not_implemented(self, iq):
 		if iq.getType() in ['set','get']:
 			self.send_error(iq)
@@ -260,7 +392,7 @@ class Transport:
 		to = event.getTo()
 		node = event.getQuerynode();
 		#Type is either 'info' or 'items'
-		if to == config.jid:
+		if to == NAME:
 			if node == None:
 				if type == 'info':
 					return self.Features
@@ -305,10 +437,10 @@ class Transport:
 			return
 		else:
 			if not fromjid.getStripped() in wz_clients: wz_clients[fromjid.getStripped()]=[]
-			if not to.getStripped() in wz_clients[fromjid.getStripped()]: 
+			if not to.getStripped() in wz_clients[fromjid.getStripped()]:
 				if '@' in to.getStripped():
 					wz_clients[fromjid.getStripped()].append(to.getStripped())
-			
+
 			wz = self.pres_exec(to, fromjid, type, show)
 			if wz: status=wz
 #			print "pres===============",fromjid,to,type,show
@@ -320,7 +452,7 @@ class Transport:
 		res = jid.getResource()
 		jid = jid.getStripped()
 		prio= 0
-		
+
 		unavail = 0
 		if type == 'unavailable':
 			show = 'unavailable'
@@ -328,13 +460,13 @@ class Transport:
 		elif not type and not show:
 			show = 'available'
 		#return show
-		
+
 		# self.online_users = {}
 		# self.online_users[jid] = {}
 		# self.online_users[jid][res] = {}
 		# self.online_users[jid][res]['prio'] = prio
 		# self.online_users[jid][res]['show'] = show
-		
+
 		if unavail:
 			if self.online_users.has_key(jid):
 				if self.online_users[jid].has_key(res):
@@ -348,7 +480,7 @@ class Transport:
 				self.online_users[jid] = {res:{'prio':prio, 'show':show}}
 			else:
 				self.online_users[jid][res] = {'prio': prio, 'show': show}
-				
+
 			maxprio = -100
 			for res in self.online_users[jid]:
 				pr = self.online_users[jid][res]['prio']
@@ -357,9 +489,9 @@ class Transport:
 					show = self.online_users[jid][res]['show']
 #			print 'self.online_users=',self.online_users
 			return show
-				
+
 	def pres_exec(self, tojid, jid, type, show):
-			
+
 		show = self.usr_show(jid, type, show)
 		jid=jid.getStripped()
 
@@ -367,7 +499,7 @@ class Transport:
 		return get_gism(tojid, short=1)
 
 	def xmpp_message(self, con, event):
-		
+
 		print "mess==============="
 		mtype = event.getType()
 		fromjid = event.getFrom()
@@ -407,11 +539,11 @@ class Transport:
 			reply(to,'хз чо за косяк')
 			return
 		cmd,params = body
-					
+
 		cmd = cmd.lower().strip()
 		params = params.lower().strip().split()
 		user = fromjid.getStripped().split('@'+self.domain)
-		
+
 		if fromjid.getStripped() != 'adminko@server.tld':
 			print fromjid.getStripped()
 			return
@@ -424,7 +556,7 @@ class Transport:
 			return
 		help = {}
 		help['config'] = 'examples:\n config enabled on\n config enabled off\n config autoremove on\n config autoremove off\n config singlecontact on\n config singlecontact off'
-		
+
 		if cmd == 'online':
 			reply(to, str(self.online_users))
 		elif cmd == 'wz':
@@ -471,7 +603,7 @@ class Transport:
 			pass
 		else:
 			reply(to,'...')
-			
+
 	def oldmails_load(self,user):
 		userpath=self.watchdir+self.domain+'/'+user+'/'
 		try:
@@ -479,20 +611,20 @@ class Transport:
 		except:
 			state={}
 		return state
-		
+
 	def oldmails_save(self,user,state):
 		try:
 			userpath=self.watchdir+self.domain+'/'+user+'/'
 			write_file(userpath+'oldmails.py',state)
 		except:
 			logError()
-			
+
 	def usercfg_load(self):
 		try:
 			self.usercfg=read_file('usercfg.py')
 		except:
 			self.usercfg={}
-			
+
 	def usercfg_save(self):
 		try:
 			write_file('usercfg.py',self.usercfg)
@@ -504,7 +636,7 @@ class Transport:
 			self.usercfg[user]={}
 		self.usercfg[user][parm]=value
 		self.usercfg_save()
-			
+
 	def usercfg_get(self,user,parm):
 		if not self.usercfg.has_key(user):
 			self.usercfg[user]={}
@@ -520,7 +652,7 @@ class Transport:
 				raise AttributeError
 		else:
 			return self.usercfg[user][parm]
-			
+
 	def mail_check(self):
 		if time.time()<transport.lastcheck: return
 		transport.lastcheck = time.time() + cache_ttl
@@ -543,16 +675,16 @@ class Transport:
 		print "checked."
 
 	def xmpp_connect(self):
-		connected = self.jabber.connect((config.mainServer,config.port))
-		if config.dumpProtocol: print "connected:",connected
+		connected = self.jabber.connect((HOST, PORT))
+#		if config.dumpProtocol: print "connected:",connected
 		while not connected:
 			time.sleep(5)
-			connected = self.jabber.connect((config.mainServer,config.port))
-			if config.dumpProtocol: print "connected:",connected
+			connected = self.jabber.connect((HOST, PORT))
+#			if config.dumpProtocol: print "connected:",connected
 		self.register_handlers()
-		if config.dumpProtocol: print "trying auth"
-		connected = self.jabber.auth(config.saslUsername,config.secret)
-		if config.dumpProtocol: print "auth return:",connected
+#		if config.dumpProtocol: print "trying auth"
+		connected = self.jabber.auth(NAME, PASSWORD)
+#		if config.dumpProtocol: print "auth return:",connected
 		return connected
 
 	def xmpp_disconnect(self):
@@ -561,18 +693,8 @@ class Transport:
 			time.sleep(5)
 			self.xmpp_connect()
 
-def loadConfig():
-	configOptions = {}
-	for configFile in config.configFiles:
-		if os.path.isfile(configFile):
-			xmlconfig.reloadConfig(configFile, configOptions)
-			config.configFile = configFile
-			return
-	print "Configuration file not found. You need to create a config file and put it in one of these locations:\n	" + "\n	".join(config.configFiles)
-	sys.exit(1)
-
 def logError():
-	err = '%s - %s\n'%(time.strftime('%a %d %b %Y %H:%M:%S'),version)
+	err = '%s - %s\n'%(time.strftime('%a %d %b %Y %H:%M:%S'), version)
 	if logfile != None:
 		logfile.write(err)
 		traceback.print_exc(file=logfile)
@@ -583,33 +705,33 @@ def logError():
 
 def sigHandler(signum, frame):
 	transport.offlinemsg = 'Signal handler called with signal %s'%signum
-	if config.dumpProtocol: print 'Signal handler called with signal %s'%signum
+#	if config.dumpProtocol: print 'Signal handler called with signal %s'%signum
 	transport.online = 0
 
 if __name__ == '__main__':
-	if 'PID' in os.environ:
-		config.pid = os.environ['PID']
-	loadConfig()
-	if config.pid:
-		pidfile = open(config.pid,'w')
-		pidfile.write(`os.getpid()`)
-		pidfile.close()
+	#if 'PID' in os.environ:
+#		config.pid = os.environ['PID']
+#	loadConfig()
+#	if config.pid:
+#		pidfile = open(config.pid,'w')
+#		pidfile.write(`os.getpid()`)
+#		pidfile.close()
 
-	if config.saslUsername:
-		sasl = 1
-	else:
-		config.saslUsername = config.jid
-		sasl = 0
+#	if config.saslUsername:
+#		sasl = 1
+#	else:
+#		config.saslUsername = config.jid
+#		sasl = 0
 
 	logfile = None
-	if config.debugFile:
-		logfile = open(config.debugFile,'a')
+#	if config.debugFile:
+#		logfile = open(config.debugFile,'a')
 
-	if config.dumpProtocol:
-		debug=['always', 'nodebuilder']
-	else:
-		debug=[]
-	connection = xmpp.client.Component(config.jid,config.port,debug=debug,sasl=sasl,bind=config.useComponentBinding,route=config.useRouteWrap)
+#	if config.dumpProtocol:
+#		debug=['always', 'nodebuilder']
+#	else:
+#		debug=[]
+	connection = xmpp.client.Component(NAME, PORT, debug=None, sasl=False, bind=False, route=False)
 	transport = Transport(connection)
 	if not transport.xmpp_connect():
 		print "Could not connect to server, or password mismatch!"
@@ -631,16 +753,13 @@ if __name__ == '__main__':
 			logError()
 		if not connection.isConnected():  transport.xmpp_disconnect()
 	connection.disconnect()
-	if config.pid:
-		os.unlink(config.pid)
-	if logfile:
-		logfile.close()
+#	if config.pid:
+#		os.unlink(config.pid)
+#	if logfile:
+#		logfile.close()
 	if transport.restart:
 		args=[sys.executable]+sys.argv
 		if os.name == 'nt': args = ["\"%s\"" % a for a in args]
 		if config.dumpProtocol: print sys.executable, args
 		os.execv(sys.executable, args)
-
-
-
 
