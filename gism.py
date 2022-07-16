b0ff4d7e0b1efdec5c2510388047d270521122d9
rain <rain@gmx.tm>
2022-07-17 00:58:07 +0300

    Implemented vcards

--- gism.py
+++ gism.py
@@ -15,6 +15,7 @@ import traceback
 import xmpp
 import xml.dom.minidom
 import sqlite3
+import re
 #import urllib2
 
 from xmpp.browser import *
@@ -44,7 +45,7 @@ def get_weather_g(kod, type):
 
             kod=str(kod)
             req = urllib2.Request(u'http://informer.gismeteo.ru/xml/'+kod+u'_1.xml',headers={'User-agent': 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)'})
-            r = urllib2.urlopen(req)
+            r = urllib2.urlopen(req, timeout=10)
 
             global embedd
             embedd=0
@@ -164,7 +165,8 @@ class Transport:
     online = 1
     offlinemsg = ''
     last = time.time()
-    version = '0.1'
+    version = '0.3'
+    weatherlogo = 'iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAADAFBMVEUAAAAXlP4Uiv8UkP4Uh/8Uhv8Ujv4LzP4MzP4M0/4Lyv8J5P4Lz/8Lzv8Vjf4Vi/4Vif4Vif4Ujf4Ui/4Uiv4Uj/4Ujf4Uk/4UkP4Tk/4Tkf4TlP4Tlv4Nwv4Nxf4Mxf4MyP4Mxv4LzP4Lyv4Lzf4MzP4L0P4Lz/4Lzv4K1P4K0v4K0f4K0P4ViP4ViP8Uiv8UjP8TjP8Uj/8Tkf8Tk/8Tlv8SmP4SmP8Smv8Rmv8Pmv8RnP8SnP8PnP8dn/E4pNY6pNQgn+4PnP4Rn/8Sn/1WrbnBw0/qyyfsyybJxUdksKoTn/kPnv8Rof8Oof9VsLzq0C7/1Rj/1Rv/1Bvw1kCKyMxoxP9ev/81r/8Tov8Qof8Qo/8apvW9zF7/2yD/2iT/2iP/2iX/5GH/8azy8cvD5/m85f+u4P9qxf8Ypv8Oov8Oo/8Rpv8Opf8zrt/m2j//4Cr/4Cv/517/9b7/9sf79cjM6vK95v+/5/9sx/8zsv8cqv8Ppf8Qpv8QqP8Op/8zsd/n4Eb/5jL/5jP/5zv/9Kj/+dL/+M/8+NHT7fPG6v/H6v/D6f+85v+75v+f3P9Mvv8RqP8Pqv8brfXA22v/7Tf/6zf/7lT/+cv/+9n/+9j2+N7U7/vP7v/Q7v/T7/9Mv/8Nqv8Qrf8NrP9ZwcHt6kr/8Vv/96n/+97//OD9++Ho9fLZ8f/c8v+m4P8ZsP8PrP8Pr/8Pr/xxy8X199D9/Ov9/On6++vu+PTi9P7i9P/j9f/O7v8vuf8Nrv8Osf8Psf8LsP9pzv/s+P3v+fvv+frq+P7q+P/r+P/s+P/V8f8vvP8Msf8PtP8Lsv971f/1+//x+v+05/8Yt/8Os/8Otv8Ltf9Fxv/j9v/2+//z+//0+//3/P/g9f9Qyv8Mtf8OuP8NuP8Quf9o0//P8P/k9v/l9//f9f+46v9QzP8Ouv8Nuv8lwf86x/87x/8zxf8Zvv8Luv8Nvf8MvP8LvP8Nv/4Nv/8Mwf8MxP8Mxv8LyP8My/8Lzf8Kz/4Kz//////VbIFxAAAALXRSTlMAAAAAAAAAAAAAAAAAABdxyPI4vvs42Re9cfrH8fHHx3H6F7042Ti++xdxyPJYUQVDAAABtElEQVR42nzGAxLDUBQAwBeOYydHqG2ljmve/xi18Xe0ABjOsBwfT3yI8xzL4BgAQQqilPxJEgWKAFpWUuk/UopMg6plEDQVdCOLYOhg5pBMsPJIFhSKSAUo3ZUrlXLpG1QvatV6o9lqd075APZVt9cfDEdjx3Vd+w14F34QRpNpNJsvlqv1xnsB24vd/qi4+ITEpOSU1LT0jMys7Gg4YMgBgdy8/ILCgqLiktKy8oryyqqq6praHAhgqAOB+obGpuYWAFPykMAAEAMAMC/vqbZt27aNrfWYItliriMSS6QyuUKhVKk1Wp2AgB4ZjCazxWqzO5wu95tH6fXRgB8FgqFwJBqLJ5JcKp3J4kCO5AvFUrlSrdW5RrPVxoAO6vb6g+FoMJ5MJ2Q2XyxxYEXWG7b9x3b7AwYcyel8ud6+7s++5OGAoSiKAuBZxekg+Hzuv5nYtm3jznayuXzhGihelcqVaq1+12i2iqVroH3T6XR7/bvBcNRpX2H8MJk+jR8wm5NmSC5ISaSWpDQse0WwLTjumuA6CHr+5i/fCyEcYVxsfxKcRcJANCaVNrv9h53RSsajOAJEXzIn7jGAuwAAAABJRU5ErkJggg=='
 
     def __init__(self,jabber):
         self.jabber = jabber
@@ -172,7 +174,7 @@ class Transport:
         self.Features = {
                         'ids':[
                             {'category':'presence','type':'text','name':"Gismeteo Weather Service"}],
-                        'features':[NS_GATEWAY, NS_DISCO_INFO, NS_VERSION, NS_LAST, NS_DISCO_ITEMS, NS_SEARCH]}
+                        'features':[NS_GATEWAY, NS_DISCO_INFO, NS_VERSION, NS_LAST, NS_DISCO_ITEMS, NS_SEARCH, NS_VCARD]}
         self.online_users = {}
 
     def register_handlers(self):
@@ -188,28 +190,28 @@ class Transport:
         print '======================= iq: ',[self], [conn], [iq]
         ns = iq.getQueryNS()
         typ = iq.getType()
-        if ns == xmpp.NS_GATEWAY:
+        if ns == xmpp.NS_GATEWAY: # to allow add bots via "Add contact"
             self.iq_gateway_handler(iq)
+        elif ns == xmpp.NS_LAST: # to get uptime
+            self.iq_last_handler(iq)
+        elif ns == xmpp.NS_SEARCH: # to allow search
+            self.iq_search_handler(iq)
+        elif ns == xmpp.NS_VERSION: # to get version
+            self.iq_version_handler(iq)
+        elif iq.getTag('vCard') and iq.getTag('vCard').getNamespace()==xmpp.NS_VCARD: # to get vcards for transport and bots
+            self.iq_vcard_handler(iq)
+        elif ns == xmpp.NS_DISCO_INFO: # to prevent error when you click on transport in service browser and allow clients to discover transport functions
+            self.iq_disco_info_handler(iq)
+        elif ns == xmpp.NS_DISCO_ITEMS: # to prevent error when you expand transport's tree in service browser
+            self.iq_disco_items_handler(iq)
 #        elif ns == xmpp.NS_STATS:
 #            self.iq_stats_handler(iq)
 #        elif ns == xmpp.NS_TIME:
 #            self.iq_time_handler(iq, 'old')
 #        elif iq.getTag('time') and iq.getTag('time').getNamespace() == xmpp.NS_NEW_TIME:
 #            self.iq_time_handler(iq, 'new')
-        elif ns == xmpp.NS_LAST:
-            self.iq_last_handler(iq)
-        elif ns == xmpp.NS_SEARCH:
-            self.iq_search_handler(iq)
-        elif ns == xmpp.NS_VERSION:
-            self.iq_version_handler(iq)
-#        elif iq.getTag('vCard') and iq.getTag('vCard').getNamespace()==xmpp.NS_VCARD:
-#            self.iq_vcard_handler(iq)
 #        elif iq.getTag('command') and iq.getTag('command').getNamespace()==xmpp.NS_COMMANDS:
 #            self.iq_command_handler(iq)
-        elif ns == xmpp.NS_DISCO_INFO:
-            self.iq_disco_info_handler(iq)
-        elif ns == xmpp.NS_DISCO_ITEMS:
-            self.iq_disco_items_handler(iq)
 #        elif iq.getTag('ping') and iq.getTag('ping').getNamespace() == xmpp.NS_PING:
 #            self.iq_ping_handler(iq)
 #        elif ns == xmpp.NS_REGISTER:
@@ -218,6 +220,51 @@ class Transport:
         else:
             print "***************************************",ns
             self.send_not_implemented(iq)
+
+    def iq_vcard_handler(self, iq):
+        repl = iq.buildReply('result')
+        query = xmpp.Node('vCard', attrs={'xmlns':xmpp.NS_VCARD})
+
+        if str(iq.getTo()) == self.domain:
+            query.setTagData(tag='NICKNAME', val='Weather')
+            query.setTagData(tag='FN',       val='Gismeteo.ru Weather Transport')
+            query.setTagData(tag='BDAY',     val='2022-07-22')
+            query.setTagData(tag='DESC',     val='Gismeteo.ru weather service')
+            query.setTagData(tag='ROLE',     val='Создаю ботов для получения погоды с gismeteo.ru')
+            query.setTagData(tag='URL',      val='https://github.com/jabberworld/gismeteo')
+
+            transav = query.addChild('PHOTO')
+            transav.setTagData(tag='BINVAL', val=self.weatherlogo)
+            transav.setTagData(tag='TYPE',   val='image/png')
+
+        else:
+            idx = str(iq.getTo().node)
+            if re.match(r"^[0-9]{1,5}$", idx):
+                data = cur.execute("SELECT * FROM cityindex WHERE idx = (?) LIMIT 1", (idx, ))
+                data = cur.fetchone()
+
+                query.setTagData(tag='URL',      val='https://github.com/jabberworld/gismeteo')
+                query.setTagData(tag='BDAY',     val='2022-07-22')
+                query.setTagData(tag='NICKNAME', val=data[6])
+
+                tel = query.addChild('TEL')
+                tel.setTagData(tag='NUMBER',     val=data[0])
+
+                addrru = query.addChild('ADR')
+                addrru.setTagData(tag='CTRY',    val=data[1]+" ("+data[3]+")")
+                addrru.setTagData(tag='REGION',  val=data[2]+" ("+data[4]+")")
+                addrru.setTagData(tag='LOCALITY',val=data[6]+" ("+data[5]+")")
+
+                query.setTagData(tag='FN',       val=data[6]+", "+data[2]+", "+data[1])
+                query.setTagData(tag='DESC',     val="Lat.: "+data[7]+", Long.: "+data[8]+", Alt.: "+data[9]+"\n"+data[10])
+
+                botav = query.addChild('PHOTO')
+                botav.setTagData(tag='BINVAL',   val=self.weatherlogo)
+                botav.setTagData(tag='TYPE',     val='image/png')
+
+        repl.setPayload([query])
+        self.jabber.send(repl)
+        raise NodeProcessed
 
     def iq_version_handler(self, iq):
         name = Node('name')
